4cd2d0e6b44c91d130977c001d3c3b26c786abf8
Cap <18879890225@163.com>
2024-06-26 10:14:11 +0800

    refactor(): 删除多余代码[]

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,28 +1,14 @@
 // index.js
 const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
 const dayjs = require('dayjs');
-// const Pitchfinder = require('pitchfinder')
 
 const wasm = require('./main.js')
 let module = null
 
-const tools = require('../../utils/util.js')
-const detectPitchYin = tools.YIN({
-    sampleRate: 44100,
-    threshold: 0.5,
-    probabilityThreshold: 0.7,
-  });
 Page({
   data: {
     msg: 'Hello World',
     record: '开始录音',
-    userInfo: {
-      avatarUrl: defaultAvatarUrl,
-      nickName: '',
-    },
-    hasUserInfo: false,
-    canIUseGetUserProfile: wx.canIUse('getUserProfile'),
-    canIUseNicknameComp: wx.canIUse('input.type.nickname'),
   },
 
   onReady: function () {
@@ -66,17 +52,13 @@ Page({
             function (res) {
               const float32Array = res.getChannelData(0);
               console.log('decodeAudioData success', float32Array)
-              // callYinPitchEstimation(float32Array, res.sampleRate);
               if (module.onRuntimeInitialized) {
                 console.log('Module.onRuntimeInitialized', module.onRuntimeInitialized)
-                module.onRuntimeInitialized = () => {
-                  runYinPitchEstimation(float32Array, res.sampleRate);
-                  // this.runCalculateIntensity(float32Array, res.sampleRate);
-                };
+                // module.onRuntimeInitialized = () => {
+                //   runYinPitchEstimation(float32Array, res.sampleRate);
+                // };
               } else {
-                console.log('222')
                 try {
-                  // this.runYinPitchEstimation(float32Array, 44100);
                   // Get the reference to the function
                   const yinPitchEstimation = module.cwrap('yinPitchEstimation', 'number', ['number', 'number']);
                   const calculateIntensity = module.cwrap('calculateIntensity', 'number', ['number', 'number']);
@@ -96,31 +78,7 @@ Page({
                 } catch (error) {
                   console.log(error)
                 }
-                
-                // this.runCalculateIntensity(float32Array, res.sampleRate);
               }
-      
-              // let source = audioCtx.createBufferSource()
-              // source.buffer = res
-              // source.connect(analyser)
-              // // source.start()
-              // let n = new Uint8Array(analyser.fftSize)
-              // analyser.getByteTimeDomainData(n)
-              // console.log('分析', analyser)
-              // console.log('分析结果', analyser.getByteTimeDomainData(n))
-              // let i = 0, r = 0, s = 0
-      
-              //     r = Math.max.apply(null, n)
-      
-              //     s = Math.min.apply(null, n)
-      
-              //     i = (r - s) / 128
-      
-              //     i = Math.round(i * 100 / 2)
-      
-              //     i = i > 100 ? 100 : i
-      
-              //     console.log('音量', i || '')
             },
             function (res) {
               console.log('decodeAudioData fail', module)
@@ -173,36 +131,4 @@ Page({
     // Free the allocated memory
     this.Module._free(inputPtr);
   },
-  // 录音数据转化为音调分析工具的入参数据
-  transformStream(buffer, audioCtx) {
-    
-  },
-  startAnys() {
-    const audioCtx = wx.createWebAudioContext()
-    const FileM =  wx.getFileSystemManager()
-    const buffer = new ArrayBuffer(14300218)
-    // FileM.open({
-    //   filePath: 'test.wav',
-    //   success: res => {
-    //     console.log(res)
-    //     FileM.read({
-    //       fd: res.fd,
-    //       arrayBuffer: buffer,
-    //       success: rs => {
-    //         audioCtx.decodeAudioData(rs, c => {
-    //           console.log(c)
-    //           // const audioData = c.getChannelData(0)
-    //           // console.log(audioData)
-    //           // const pitch = detectPitchYin(audioData)
-    //           // console.log(pitch)
-    //         }, err => {
-    //           console.error('decodeAudioData fail', err)
-    //         })
-    //       }
-    //     })
-    //   }
-    // })
-  },
-  loadWasm() {
-  }
 })
